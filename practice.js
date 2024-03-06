54b396c1f81c0647e570b30d56d37b565d57ae80
unknown <adarsh@cfcsindia.com>
2024-03-06 18:31:35 +0530

    Student Management System

--- practice.js
+++ practice.js
@@ -1,12 +1,25 @@
-
+let id=0;
+function myFunction() {
+const StudentName=document.getElementById("Name").value;
+const mathMark=document.getElementById("MathMarks").value
+const Scince_Mark=document.getElementById("Science_Marks").value
+const English_Mark=document.getElementById("English_Marks").value
+const Hindi_Mark=document.getElementById("Hindi_Marks").value
+const ComputerMark=document.getElementById("computer_science_marks").value
+const Gender=document.getElementById("gender").value
 let students =[];
-function create(id,StudentName,mathMark,Scince_Mark,English_Mark,Hindi_Mark,ComputerMark,Gender){
-    var a={StudentId:id,name:StudentName,maths_marks:mathMark,science_marks:Scince_Mark,english_marks:English_Mark,hindi_marks:Hindi_Mark,computer_science_marks:ComputerMark,gender:Gender};
+function create(StudentName,mathMark,Scince_Mark,English_Mark,Hindi_Mark,ComputerMark,Gender){
+    id++;
+    var status=true;
+    var a={StudentId:id,name:StudentName,maths_marks:mathMark,science_marks:Scince_Mark,english_marks:English_Mark,hindi_marks:Hindi_Mark,computer_science_marks:ComputerMark,gender:Gender,status:status};
     students.push(a);
 }
 function Read(){
     for(let i=0;i<students.length;i++){
-        console.log(students[i]);
+        if(students[i].status==true){
+            console.log(JSON.stringify(students[i]));
+        }
+       
     }
 }
 function update(id,StudentName,mathMark,Scince_Mark,English_Mark,Hindi_Mark,ComputerMark,Gender){
@@ -21,17 +34,42 @@ function update(id,StudentName,mathMark,Scince_Mark,English_Mark,Hindi_Mark,Comp
             students[i].gender=Gender;
         }
     }
-
 }
 function del(id){
     for(let i=0;i<students.length;i++){
         if(students[i].StudentId==id){
-            students.splice(i,1);
+            students[i].status=false;
+            // students.splice(i,1);
         }
     }
 }
-create(1,"Ajay",90,90,90,100,89,"male")
-create(4,"Abhinav",90,90,90,100,89,"male")
-update(4,"Adarsh",90,90,90,100,89,"male")
-Read();
-console.log(students);
+create(StudentName,mathMark,Scince_Mark,English_Mark,Hindi_Mark,ComputerMark,Gender);
+// Read();
+// del(1);
+// Read(); 
+  let tbl = document.createElement("table");
+  console.log(tbl);
+ 
+  const tblBody = document.createElement("tbody");
+  for (let i = 0; i < students.length; i++) {
+    const row = document.createElement("tr");
+    const exampleObj=students[i];
+for (let key in exampleObj) {
+    if (exampleObj.hasOwnProperty(key)) {
+        const cell = document.createElement("td");
+        value = exampleObj[key];
+        const cellText = document.createTextNode(`${value}`);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+    }
+}
+    tblBody.appendChild(row);
+  }
+  tbl.appendChild(tblBody);
+  document.body.appendChild(tbl);
+  tbl.setAttribute("border", "2");
+    
+//    var x=document.getElementById("Name").value;
+//    document.getElementById("ab").innerHTML=x
+//    console.log(x);
+  }
